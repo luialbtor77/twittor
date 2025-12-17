2e9bc40a8bb3ed5b2cc0f2c2b0a61cc90ce6c1f3
luis alba <luialbtor@gmail.com>
2025-12-17 21:31:26 +0100

    modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@ importScripts('js/sw-utils.js');
 
 /** Configuración Service Worker */
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 // APP_SHELL
@@ -74,7 +74,11 @@ self.addEventListener('activate', e => {
 
         keys.forEach( key => {
 
-            if (  key != STATIC_CACHE && key.includes('static') ) {
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
 
